9fa5357396afc7229ee48ce9cb03f9cd03aa47b6
Alexander Morozov <a.morozov@picodata.io>
2024-11-22 19:02:53 +0300

    Add integration test for cargo run

---
+++ tests/run.rs
@@ -0,0 +1,9 @@
+mod helpers;
+use helpers::helpers::run_cluster;
+use std::time::Duration;
+
+const TOTAL_INSTANCES: i32 = 4;
+#[test]
+fn test_cluster_setup_debug() {
+    let _cluster_handle = run_cluster(Duration::from_secs(120), TOTAL_INSTANCES).unwrap();
+}
