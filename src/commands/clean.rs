1453168f528748e9f5691ef94970b8b53949c5f4
Alexander Morozov <a.morozov@picodata.io>
2024-12-05 18:15:05 +0300

    Improve clean command error handling

--- src/commands/clean.rs
+++ src/commands/clean.rs
@@ -1,11 +1,16 @@
 use anyhow::{Context, Result};
-use log::info;
+use log::{info, warn};
 use std::{fs, path::Path};
 
 pub fn cmd(data_dir: &Path) -> Result<()> {
-    info!("Clearing cluster data directory");
-    fs::remove_dir_all(data_dir)
-        .context(format!("failed to remove directory {}", data_dir.display()))?;
+    info!("Clearing cluster data directory:");
+    if data_dir.exists() {
+        fs::remove_dir_all(data_dir)
+            .context(format!("failed to remove directory {}", data_dir.display()))?;
+        info!("Successfully removed : {}", data_dir.to_string_lossy());
+    } else {
+        warn!("Data directory does not exist")
+    }
 
     Ok(())
 }
