638d8c1076e100f1d1de6622a3c20ed6e1982237
lowit <ea@lowit.ru>
2025-01-24 13:21:56 +0300

    feat: add stopping cluster in clean cmd

--- src/commands/clean.rs
+++ src/commands/clean.rs
@@ -1,9 +1,11 @@
+use crate::commands;
 use anyhow::{Context, Result};
 use log::{info, warn};
 use std::{fs, path::Path};
 
 pub fn cmd(data_dir: &Path) -> Result<()> {
     info!("Clearing cluster data directory:");
+    let _ = commands::stop::cmd(data_dir).context("failed stop cluster before clean");
     if data_dir.exists() {
         fs::remove_dir_all(data_dir)
             .context(format!("failed to remove directory {}", data_dir.display()))?;
