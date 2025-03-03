3bd696265e6c06177b0dd652089dc17f540a14a4
Boris Sinyapkin <b.sinyapkin@picodata.io>
2025-03-03 19:19:48 +0300

    feat: expose PicodataInstance object

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub mod cluster {
     pub use crate::commands::run::ParamsBuilder as RunParamsBuilder;
 
     pub use crate::commands::run::MigrationContextVar;
+    pub use crate::commands::run::PicodataInstance;
     pub use crate::commands::run::Plugin;
     pub use crate::commands::run::Service;
     pub use crate::commands::run::Tier;
