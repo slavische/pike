655dd637e3e26ff00011c55600a3adec3304fe60
dodokek <alex.morozov.dd@mail.ru>
2025-04-24 15:51:40 +0300

    feat: add hello http-endpoint to template

--- plugin_template/src/service.rs
+++ plugin_template/src/service.rs
@@ -1,6 +1,22 @@
+use std::collections::HashMap;
+
 use crate::config;
 
+use anyhow;
+use once_cell::sync::Lazy;
 use picodata_plugin::plugin::prelude::*;
+use shors::transport::{
+    http::{
+        route::{Builder, Route},
+        server::Server,
+        Request, Response,
+    },
+    Context,
+};
+
+thread_local! {
+    static HTTP_SERVER: Lazy<Server> = Lazy::new(Server::new);
+}
 
 #[derive(Debug)]
 pub struct PluginService {}
@@ -29,6 +45,13 @@ impl Service for PluginService {
     fn on_start(&mut self, context: &PicoContext, config: Self::Config) -> CallbackResult<()> {
         _ = context;
         _ = config.unwrap_or_default();
+
+        HTTP_SERVER.with(|srv| {
+            routes()
+                .into_iter()
+                .for_each(|route| srv.register(Box::new(route)));
+        });
+
         Ok(())
     }
 
@@ -46,4 +69,43 @@ impl Service for PluginService {
         _ = context;
         Ok(())
     }
+}
+
+pub fn routes() -> Vec<Route<anyhow::Error>> {
+    let hello_route = Builder::new().with_method("GET").with_path("/hello").build(
+        |_: &mut Context, _: Request| -> anyhow::Result<_> {
+            let message: &str = r"
+     _________________________________
+    / Hello there! I see that you've  \
+    | successfully deployed picodata  |
+    | app using pike. Congrats! Type  |
+    \ cargo pike --help for more tips /
+     ---------------------------------
+                    |
+                    |
+                   ,|.
+                  ,\|/.
+                ,' .V. `.
+               / .     . \
+              /_`       '_\
+             ,' .:     ;, `.
+             |@)|  . .  |(@|
+        ,-._ `._';  .  :`_,' _,-.
+       '--  `-\ /,-===-.\ /-'  --`
+      (----  _|  ||___||  |_  ----)
+       `._,-'  \  `-.-'  /  `-._,'
+                `-.___,-'
+     ";
+            Ok(Response {
+                status: 200,
+                headers: HashMap::from([(
+                    "content-type".to_string(),
+                    "application/json; charset=utf8".to_string(),
+                )]),
+                body: message.as_bytes().to_vec(),
+            })
+        },
+    );
+
+    vec![hello_route]
 }
