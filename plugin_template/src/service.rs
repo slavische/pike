93797886468d35b4aebce4d127b9883768ab07f2
Aleksander Morozov <alex.morozov.dd@mail.ru>
2025-04-29 20:30:45 +0300

    feat: add rpc handle examples to template

--- plugin_template/src/service.rs
+++ plugin_template/src/service.rs
@@ -3,8 +3,10 @@ use std::collections::HashMap;
 use crate::config;
 
 use once_cell::sync::Lazy;
-use picodata_plugin::plugin::prelude::*;
 use picodata_plugin::system::tarantool::log as t_log;
+use picodata_plugin::transport::rpc;
+use picodata_plugin::{plugin::prelude::*, transport::rpc::RouteBuilder};
+use serde::{Deserialize, Serialize};
 use shors::transport::{
     Context,
     http::{
@@ -14,6 +16,16 @@ use shors::transport::{
     },
 };
 use std::sync::LazyLock;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct User {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ExampleResponse {
+    data: String,
+}
 
 static LOGGER: LazyLock<t_log::TarantoolLogger> = LazyLock::new(t_log::TarantoolLogger::default);
 
@@ -49,6 +61,7 @@ impl Service for PluginService {
         _ = ctx;
         _ = new_config.unwrap_or_default();
         _ = old_config.unwrap_or_default();
+
         Ok(())
     }
 
@@ -64,6 +77,25 @@ impl Service for PluginService {
                 .into_iter()
                 .for_each(|route| srv.register(Box::new(route)));
         });
+
+        log::warn!("Registering RPC handle /test");
+
+        RouteBuilder::from_pico_context(context)
+            .path("/test")
+            .register(move |req, _ctx| {
+                log::debug!("Received store request: {req:?}");
+
+                let user: User = rmp_serde::from_slice(req.as_bytes()).unwrap();
+                log::warn!("Recieved \"{user:?}\" as RPC input");
+
+                let user_name = user.name;
+                let response_to_return = ExampleResponse {
+                    data: format!("Hello {user_name}, long time no see."),
+                };
+
+                Ok(rpc::Response::encode_rmp(&response_to_return).unwrap())
+            })
+            .unwrap();
 
         Ok(())
     }
@@ -84,6 +116,7 @@ impl Service for PluginService {
     }
 }
 
+#[must_use]
 pub fn routes() -> Vec<Route<anyhow::Error>> {
     let hello_route = Builder::new().with_method("GET").with_path("/hello").build(
         |_: &mut Context, _: Request| -> anyhow::Result<_> {
--- plugin_template/tests/integration.rs
+++ plugin_template/tests/integration.rs
@@ -1,9 +1,9 @@
-use reqwest::blocking as req;
 use picotest::*;
+use reqwest::blocking as req;
 
 #[picotest]
 fn test_cluster_handles() {
-    let http_port = cluster.instances().first().unwrap().http_port;
+    let http_port = cluster.main().properties().http_port;
 
     let resp = req::get(format!("http://127.0.0.1:{http_port}/metrics")).unwrap();
     assert!(resp.status().is_success());
